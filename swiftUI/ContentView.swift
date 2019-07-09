d361d7d996bc6e7097a285757d90d57521ea0d7b
Edouard Topin <ed.topin@gmail.com>
2019-07-09 15:58:28 +0200

    Initial Commit

---
+++ swiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  swiftUI
+//
+//  Created by Edouard Topin on 09/07/2019.
+//  Copyright © 2019 Edouard Topin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
